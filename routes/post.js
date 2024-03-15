b6050230e9d4428d978cb72a0c7b5ea798935261
thedevyash <imailsharma.yash@gmail.com>
2024-03-15 21:59:20 +0530

    likes/unlike

--- routes/post.js
+++ routes/post.js
@@ -99,7 +99,7 @@ postRouter.post("/api/do-like", async (req,res) =>{
 }
 else
 {
-    await Post.findOneAndUpdate({_id:req.body.post},{$unset:{likes:like}} );
+    await Post.findOneAndUpdate({_id:req.body.post},{$pull:{likes:like}} );
 return res.status(200).json({"mssg":"Post was Unliked Successfully"});
 }}catch(e)
 {
